e31db4669369d54290682dbd81e5bb036a74c5ad
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-12-14 15:43:31 +0100

    added: dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file was created by Translation Editor v7.0-beta
+ * On 2018-12-14 15:42
+ */
+
+return array (
+  'redirector' => 'Redirector',
+  'redirector:settings:info' => 'Configureer URLs die je wilt doorsturen naar een andere URL.',
+  'redirector:settings:redirects:add:placeholder' => 'Voer een URL in',
+  'redirector:settings:redirects:add:title' => 'Nieuwe redirect toevoegen',
+  'redirector:settings:redirects:list:title' => 'Lijst van huidige redirects',
+);
